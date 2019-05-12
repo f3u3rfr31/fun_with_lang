66d6ff4ca3533e94adb808f38e5aee795fcbcf71
David <davidjefischer@gmail.com>
2019-05-11 23:57:23 -0400

    first com

---
+++ test.py
@@ -0,0 +1,16 @@
+import threading
+
+con = True
+
+alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+
+def get_chars(sum):
+    chars[] = array()
+    
+    
+if __name__ = "__main__":
+    if len(sys.argv < 3):
+        to_print = get_chars(sys.argv[1])
+        for toP in to_print:
+            print(toP)
+
